ce955551f69cdf2a6c3b6c033539962b8a352ce0
roneron808-lang <roneron808@gmail.com>
2025-12-16 23:39:09 +0800

    assignment3

---
+++ assignment3/question6.js
@@ -0,0 +1,30 @@
+const disneyData = {
+	open: "120.54",
+	high: "122.56",
+	low: "120.54",
+	close: "121.09",
+	volume: "90000"
+};
+
+// Write your code below
+const open = parseFloat(disneyData['open']);
+const high = parseFloat(disneyData['high']);
+const low = parseFloat(disneyData['low']); 
+const close = parseFloat(disneyData['close']);
+const volume = parseFloat(disneyData['volume']);
+
+if(close > open){
+    if(volume > 100000){
+        console.log('Strong Bullish');
+    }else{
+        console.log('Bullish');
+    }
+}else if(close < open){
+    if(volume > 10000){
+        console.log('Strong Bearish');
+    }else{
+        console.log('Bearish');
+    }   
+}else{
+    console.log('neutral');
+}
